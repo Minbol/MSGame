1e9a371eb6f9ffd5fbc5e2cb00dc1042bd93b9de
erling0429 <erlingh0324@gmail.com>
2026-06-16 20:21:28 +0900

    [erling] VRM 플러그인

---
+++ PlugIns/VRM4U-master/Source/ReleaseScript/build_5.bat
@@ -0,0 +1,100 @@
+set V_DATE=%date:~0,4%%date:~5,2%%date:~8,2%
+
+set BUILD_SCRIPT=build_ver2.bat
+
+::5_8
+call %BUILD_SCRIPT% 5.8 Win64 Shipping VRM4U_5_8_%V_DATE%.zip
+if not %errorlevel% == 0 (
+    echo [ERROR] :P
+    goto err
+)
+call %BUILD_SCRIPT% 5.8 Android Development VRM4U_5_8_%V_DATE%_android.zip
+if not %errorlevel% == 0 (
+    echo [ERROR] :P
+    goto err
+)
+call %BUILD_SCRIPT% 5.8 Linux Shipping VRM4U_5_8_%V_DATE%_linux.zip
+if not %errorlevel% == 0 (
+    echo [ERROR] :P
+    goto err
+)
+
+
+::5_7
+call %BUILD_SCRIPT% 5.7 Win64 Shipping VRM4U_5_7_%V_DATE%.zip
+if not %errorlevel% == 0 (
+    echo [ERROR] :P
+    goto err
+)
+call %BUILD_SCRIPT% 5.7 Android Development VRM4U_5_7_%V_DATE%_android.zip
+if not %errorlevel% == 0 (
+    echo [ERROR] :P
+    goto err
+)
+call %BUILD_SCRIPT% 5.7 Linux Shipping VRM4U_5_7_%V_DATE%_linux.zip
+if not %errorlevel% == 0 (
+    echo [ERROR] :P
+    goto err
+)
+
+::5_6
+call %BUILD_SCRIPT% 5.6 Win64 Shipping VRM4U_5_6_%V_DATE%.zip
+if not %errorlevel% == 0 (
+    echo [ERROR] :P
+    goto err
+)
+call %BUILD_SCRIPT% 5.6 Android Development VRM4U_5_6_%V_DATE%_android.zip
+if not %errorlevel% == 0 (
+    echo [ERROR] :P
+    goto err
+)
+
+::5_5
+call %BUILD_SCRIPT% 5.5 Win64 Shipping VRM4U_5_5_%V_DATE%.zip
+if not %errorlevel% == 0 (
+    echo [ERROR] :P
+    goto err
+)
+call %BUILD_SCRIPT% 5.5 Win64 Debug VRM4U_5_5_%V_DATE%_debug.zip
+if not %errorlevel% == 0 (
+    echo [ERROR] :P
+    goto err
+)
+
+
+::5_4
+call %BUILD_SCRIPT% 5.4 Win64 Shipping VRM4U_5_4_%V_DATE%.zip
+if not %errorlevel% == 0 (
+    echo [ERROR] :P
+    goto err
+)
+
+::call build_ver.bat 5.4 Win64 Development MyProjectBuildScriptEditor VRM4U_5_4_%V_DATE%.zip
+::if not %errorlevel% == 0 (
+::    echo [ERROR] :P
+::    goto err
+::)
+
+
+::5_3
+call %BUILD_SCRIPT% 5.3 Win64 Shipping VRM4U_5_3_%V_DATE%.zip
+if not %errorlevel% == 0 (
+    echo [ERROR] :P
+    goto err
+)
+
+::5_2
+call %BUILD_SCRIPT% 5.2 Win64 Shipping VRM4U_5_2_%V_DATE%.zip
+if not %errorlevel% == 0 (
+    echo [ERROR] :P
+    goto err
+)
+
+
+:finish
+exit /b 0
+
+:err
+exit /b 1
+
+
